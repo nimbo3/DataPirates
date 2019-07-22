a62c7fab7e6c87b6c5cd88b420d14f22e1d10e54
Hamidreza Sharifzadeh <hamidrezash2000@gmail.com>
2019-07-22 12:02:39 +0430

    Init Database Architecture

---
+++ crawler/src/main/java/in/nimbo/model/Site.java
@@ -0,0 +1,59 @@
+package in.nimbo.model;
+
+import java.util.List;
+
+public class Site {
+    private String link;
+    private String title;
+    private String keywords;
+    private String plainText;
+    private List<String> anchors;
+
+    public Site() {
+    }
+
+    public Site(String link, String title) {
+        this.link = link;
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getPlainText() {
+        return plainText;
+    }
+
+    public void setPlainText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    public List<String> getAnchors() {
+        return anchors;
+    }
+
+    public void setAnchors(List<String> anchors) {
+        this.anchors = anchors;
+    }
+}
