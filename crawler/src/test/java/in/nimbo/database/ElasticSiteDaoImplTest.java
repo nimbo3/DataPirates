9d5f2b86d903d174109d536c182b5efb375acfda
alireza <asady529@gmail.com>
2019-07-29 17:16:36 +0430

    fix and refactor tests

--- crawler/src/test/java/in/nimbo/database/ElasticSiteDaoImplTest.java
+++ crawler/src/test/java/in/nimbo/database/ElasticSiteDaoImplTest.java
@@ -1,5 +1,6 @@
 package in.nimbo.database;
 
+import com.codahale.metrics.SharedMetricRegistries;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import in.nimbo.dao.ElasticSiteDaoImpl;
@@ -8,26 +9,29 @@ import in.nimbo.model.Site;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ElasticSiteDaoImplTest {
 
     private static ElasticSiteDaoImpl elasticDao;
+    private static Config config = ConfigFactory.load("config");
+
     @BeforeClass
     public static void prepareElasticClient() {
+        try {
+            SharedMetricRegistries.getDefault();
+        } catch (IllegalStateException e) {
+            SharedMetricRegistries.setDefault(config.getString("metric.registry.name"));
+        }
         Config config = ConfigFactory.load("config");
         elasticDao = new ElasticSiteDaoImpl(config);
     }
 
     @Test
-    public void insertTest() {
+    public void insertTest() throws SiteDaoException {
         final String LINK = "http://test.test";
         Site site = new Site(LINK, "Test Title");
-        try {
-            elasticDao.insert(site);
-        } catch (SiteDaoException e) {
-            e.printStackTrace();
-        }
-        assertTrue(elasticDao.get(LINK).equals(site));
+        elasticDao.insert(site);
+        assertEquals(elasticDao.get(LINK), site);
     }
 }
