910802c53e2a20392ec7765bef21c404fbd8f06b
alireza <asady529@gmail.com>
2019-08-01 20:18:24 +0430

    add insert test for hbase

--- crawler/src/test/java/in/nimbo/dao/HbaseSiteDaoImplTest.java
+++ crawler/src/test/java/in/nimbo/dao/HbaseSiteDaoImplTest.java
@@ -4,18 +4,46 @@ package in.nimbo.dao;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import in.nimbo.exception.HbaseSiteDaoException;
+import in.nimbo.exception.SiteDaoException;
+import in.nimbo.model.Site;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class HbaseSiteDaoImplTest {
+    private static HbaseSiteDaoImpl hbaseSiteDao;
 
-    @Test
-    public void insert() throws HbaseSiteDaoException {
+    @BeforeClass
+    public static void init() throws HbaseSiteDaoException {
         Configuration hBaseConfiguration = HBaseConfiguration.create();
         Config config = ConfigFactory.load("config");
-        HbaseSiteDaoImpl hbaseSiteDao = new HbaseSiteDaoImpl(hBaseConfiguration, config);
+        hbaseSiteDao = new HbaseSiteDaoImpl(hBaseConfiguration, config);
+    }
+
+    @Test
+    public void insert() throws SiteDaoException {
+        Site site = new Site("www.google.com", "welcome to google!");
+        Map<String, String> map = new HashMap<>();
+        map.put("www.stackoverflow.com/google", "see stack");
+        map.put("www.yahoo.com/google", "see yahoo");
+        site.setAnchors(map);
+        site.setReverseLink("com.google.www");
+        hbaseSiteDao.insert(site);
+        NavigableMap<byte[], byte[]> actualByteMap = hbaseSiteDao.get(site.getReverseLink()).getFamilyMap(Bytes.toBytes("links"));
+        Map<String, String> actual = new HashMap<>();
+        for (Map.Entry<byte[], byte[]> entry : actualByteMap.entrySet()) {
+            actual.put(new String(entry.getKey()), new String(entry.getValue()));
+        }
+        assertEquals(site.getAnchors(), actual);
     }
 
     @Test
