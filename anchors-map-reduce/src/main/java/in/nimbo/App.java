ff1e28c91e7203f383e2518472e646549de4bf9a
Kazem Faghih <kazemf78@yahoo.com>
2019-08-01 19:59:47 +0430

    add basic implementation for spark map/reduce

--- anchors-map-reduce/src/main/java/in/nimbo/App.java
+++ anchors-map-reduce/src/main/java/in/nimbo/App.java
@@ -4,6 +4,8 @@ package in.nimbo;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
@@ -37,6 +39,7 @@ public class App {
         hbaseConfiguration.addResource(hbaseXmlHbase);
         hbaseConfiguration.set(TableInputFormat.INPUT_TABLE, hbaseTableName);
         hbaseConfiguration.set(TableInputFormat.SCAN_COLUMN_FAMILY, hbaseColumnFamily);
+        hbaseConfiguration.set(TableInputFormat.SCAN_CACHEDROWS, "500");
 
         SparkConf sparkConf = new SparkConf();
         sparkConf.setAppName(sparkAppName);
@@ -45,20 +48,11 @@ public class App {
         JavaRDD<Result> hbaseRDD = sparkContext.newAPIHadoopRDD(hbaseConfiguration
                 , TableInputFormat.class, ImmutableBytesWritable.class, Result.class)
                 .values();
+        JavaRDD<Cell> cellRDD = hbaseRDD.flatMap(result -> result.listCells().iterator());
+        JavaPairRDD<byte[], Integer> linkToOne = cellRDD.mapToPair(cell -> new Tuple2<>(CellUtil.cloneQualifier(cell), 1));
+        JavaPairRDD<byte[], Integer> linkToCount = linkToOne.reduceByKey(Integer::sum);
 
-        JavaPairRDD<String, ArrayList<String>> mapResult = hbaseRDD.flatMapToPair((PairFlatMapFunction<Result, String, ArrayList<String>>) row -> {
-            ArrayList<Tuple2<String, ArrayList<String>>> result = new ArrayList<>();
-            NavigableMap<byte[], byte[]> familyMap = row.getFamilyMap(Bytes.toBytes(hbaseColumnFamily));
-            familyMap.forEach((qualifier, value) -> {
-                String link = Bytes.toString(qualifier);
-                String text = Bytes.toString(value);
-                System.out.println("link: " + link);
-                System.out.println("text: " + text);
-                result.add(new Tuple2<>(link, new ArrayList<>(Collections.singletonList(text))));
-            });
-            return result.iterator();
-        });
-        System.out.println(mapResult.count());
+        linkToCount.foreach(t -> System.out.println(new String(t._1) + ":" + t._2));
         sparkContext.close();
 
     }
