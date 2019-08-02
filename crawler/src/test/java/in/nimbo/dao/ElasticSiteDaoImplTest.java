ede2dce23df162fa099343d95deaf78f27bef91d
Hamidreza Sharifzadeh <hamidrezash2000@gmail.com>
2019-08-02 09:56:57 +0430

    add insert Test for elastic

---
+++ crawler/src/test/java/in/nimbo/dao/ElasticSiteDaoImplTest.java
@@ -0,0 +1,58 @@
+package in.nimbo.dao;
+
+import com.codahale.metrics.SharedMetricRegistries;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import in.nimbo.exception.ElasticSiteDaoException;
+import in.nimbo.exception.SiteDaoException;
+import in.nimbo.model.Site;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ElasticSiteDaoImplTest {
+    private static ElasticSiteDaoImpl elasticDao;
+    @BeforeClass
+    public static void prepareElasticClient() {
+        Config config = ConfigFactory.load("config");
+        try {
+            SharedMetricRegistries.getDefault();
+        } catch (IllegalStateException e) {
+            SharedMetricRegistries.setDefault(config.getString("metric.registry.name"));
+        }
+        elasticDao = new ElasticSiteDaoImpl(config);
+    }
+
+    @Test
+    public void insertTest() throws ElasticSiteDaoException {
+        final String LINK = "http://test.test";
+        Site site = new Site(LINK, "Test Title");
+        site.setKeywords("keywords");
+        site.setPlainText("text");
+        elasticDao.insert(site);
+        assertTrue(elasticDao.get(LINK).equals(site));
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void insertLinkSizeUpperThan512BytesTest() throws ElasticSiteDaoException {
+        expectedException.expect(ElasticSiteDaoException.class);
+        expectedException.expectMessage("Elastic Long Id Exception (bytes of id must be lower than 512 bytes)");
+        final String LINK = "http://aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaa.test";
+        Site site = new Site(LINK, "Test Title");
+        site.setKeywords("keywords");
+        site.setPlainText("text");
+        elasticDao.insert(site);
+    }
+}
