5b2b4c84a7343779773e3b78a30b43ec94a32b4d
Kazem Faghih <kazemf78@yahoo.com>
2019-08-02 14:33:27 +0430

    add accumulator for cell and row count

--- anchors-map-reduce/src/main/java/in/nimbo/App.java
+++ anchors-map-reduce/src/main/java/in/nimbo/App.java
@@ -14,7 +14,10 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.LongAccumulator;
 import scala.Tuple2;
+
+import java.util.List;
 
 
 public class App {
@@ -25,6 +28,10 @@ public class App {
         String hbaseXmlHbase = config.getString("hbase.xml.url.in.hbase");
         String hbaseTableName = config.getString("hbase.table.name");
         String hbaseColumnFamily = config.getString("hbase.column.family");
+        String sparkExecutorCores = config.getString("spark.executor.cores");
+        String sparkExecutorMemory = config.getString("spark.executor.memory");
+
+
 
         Configuration hbaseConfiguration = HBaseConfiguration.create();
 
@@ -35,23 +42,31 @@ public class App {
         hbaseConfiguration.set(TableInputFormat.SCAN_COLUMN_FAMILY, hbaseColumnFamily);
         hbaseConfiguration.set(TableInputFormat.SCAN_CACHEDROWS, "500");
 
-        SparkConf sparkConf = new SparkConf();
-        sparkConf.setAppName(sparkAppName);
+        SparkConf sparkConf = new SparkConf()
+                .setAppName(sparkAppName)
+                .set("spark.executor.cores", sparkExecutorCores)
+                .set("spark.executor.memory", sparkExecutorMemory);
+
+
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        LongAccumulator rowCount = sparkContext.sc().longAccumulator();
+        LongAccumulator cellCount = sparkContext.sc().longAccumulator();
 
         JavaRDD<Result> hbaseRDD = sparkContext.newAPIHadoopRDD(hbaseConfiguration
                 , TableInputFormat.class, ImmutableBytesWritable.class, Result.class)
                 .values();
         JavaRDD<Cell> cellRDD = hbaseRDD.flatMap(result -> {
-            result.listCells().forEach(t -> System.out.println("String(qualifier) = " + new String(CellUtil.cloneQualifier(t)) + " :: String(value) = " + new String(CellUtil.cloneValue(t)) +
-                    ",\t" + CellUtil.cloneQualifier(t) + " :: " + CellUtil.cloneValue(t)));
-            return result.listCells().iterator();
+            List<Cell> cells = result.listCells();
+            rowCount.add(1);
+            cellCount.add(cells.size());
+            return cells.iterator();
         });
-        System.out.println(cellRDD.count());
-//        JavaPairRDD<byte[], Integer> linkToOne = cellRDD.mapToPair(cell -> new Tuple2<>(CellUtil.cloneQualifier(cell), 1));
-//        JavaPairRDD<byte[], Integer> linkToCount = linkToOne.reduceByKey(Integer::sum);
+        JavaPairRDD<byte[], Integer> linkToOne = cellRDD.mapToPair(cell -> new Tuple2<>(CellUtil.cloneQualifier(cell), 1));
+        JavaPairRDD<byte[], Integer> linkToCount = linkToOne.reduceByKey(Integer::sum);
 
-//        linkToCount.foreach(t -> System.out.println(new String(t._1) + ":" + t._2));
+        linkToCount.foreach(t -> System.out.println(new String(t._1) + ":" + t._2));
+
         sparkContext.close();
 
     }
