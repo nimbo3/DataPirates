45369f9e47e91c05d58161d0b1e25aaeb51bce27
Hamidreza Sharifzadeh <hamidrezash2000@gmail.com>
2019-08-06 22:24:06 +0430

    refactor elastic

--- crawler/src/main/java/in/nimbo/dao/ElasticSiteDaoImpl.java
+++ crawler/src/main/java/in/nimbo/dao/ElasticSiteDaoImpl.java
@@ -1,11 +1,12 @@
 package in.nimbo.dao;
 
 import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
 import com.codahale.metrics.Timer;
 import com.typesafe.config.Config;
-import in.nimbo.exception.ElasticSiteDaoException;
 import in.nimbo.exception.ElasticLongIdException;
+import in.nimbo.exception.ElasticSiteDaoException;
 import in.nimbo.model.Site;
 import org.apache.http.HttpHost;
 import org.apache.log4j.Logger;
@@ -21,7 +22,6 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -32,75 +32,78 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class ElasticSiteDaoImpl implements SiteDao, Closeable {
-    private static Logger logger = Logger.getLogger(ElasticSiteDaoImpl.class);
-    private final Config config;
+    private final Logger logger = Logger.getLogger(ElasticSiteDaoImpl.class);
     private final Timer insertionTimer;
     private final Meter elasticFailureMeter;
     private final Timer deleteTimer;
-    private RestHighLevelClient restHighLevelClient;
-    private BulkProcessor bulkProcessor;
+    private final Timer bulkInsertionTimer;
+    private final Meter bulkInsertionMeter;
+    private final Meter bulkInsertionFailures;
     private String index;
-    private String hostname;
-    private int port;
+    private int elasticBulkTimeOut;
+    private RestHighLevelClient client;
+    private BulkProcessor bulkProcessor;
 
-    private BulkProcessor.Listener listener = new BulkProcessor.Listener() {
+    private BulkProcessor.Listener bulkProcessorListener = new BulkProcessor.Listener() {
+        Timer.Context bulkInsertTime;
+
         @Override
         public void beforeBulk(long executionId, BulkRequest request) {
             logger.info("Sending bulk request ...");
+            bulkInsertionMeter.mark(request.requests().size());
+            bulkInsertTime = bulkInsertionTimer.time();
         }
 
         @Override
         public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
-            //Todo : Handle failures in response
-            logger.info("Bulk request sent.");
+            logger.info("Bulk request sent successfully.");
+            bulkInsertTime.stop();
         }
 
         @Override
         public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
-            logger.error("Bulk request failed !", failure);
+            logger.error("Bulk request failed.", failure);
+            bulkInsertTime.stop();
+            bulkInsertionFailures.mark();
         }
     };
 
     public ElasticSiteDaoImpl(Config config) {
-        this.config = config;
-        insertionTimer = SharedMetricRegistries.getDefault().timer(config.getString("elastic.insertion.metric.name"));
-        elasticFailureMeter = SharedMetricRegistries.getDefault().meter(config.getString("elastic.insertion.failure.metric.name"));
-        deleteTimer = SharedMetricRegistries.getDefault().timer(config.getString("elastic.delete.metric.name"));
-        this.hostname = config.getString("elastic.hostname");
-        this.port = config.getInt("elastic.port");
         this.index = "sites";
-        BulkProcessor.Builder bulkProcessorBuilder = BulkProcessor.builder(
-                (bulkRequest, bulkResponseActionListener) ->
-                        getClient().bulkAsync(bulkRequest, RequestOptions.DEFAULT,
-                                bulkResponseActionListener), listener);
-        bulkProcessorBuilder.setBulkActions(config.getInt("elastic.bulk.size"));
-        bulkProcessorBuilder.setConcurrentRequests(config.getInt("elastic.concurrent.requests"));
-        bulkProcessorBuilder.setFlushInterval(
-                TimeValue.timeValueMinutes(config.getInt("elastic.bulk.flush.interval.seconds")));
-        bulkProcessorBuilder.setBackoffPolicy(BackoffPolicy.constantBackoff(
-                TimeValue.timeValueSeconds(config.getLong("elastic.backoff.delay.seconds")),
-                config.getInt("elastic.backoff.retries")));
+        this.elasticBulkTimeOut = config.getInt("elastic.bulk.timeout");
+        this.client = new RestHighLevelClient(RestClient.builder(
+                new HttpHost(config.getString("elastic.hostname"),
+                        config.getInt("elastic.port"))));
+
+        BulkProcessor.Builder bulkProcessorBuilder = BulkProcessor.builder((bulkRequest, bulkResponseActionListener) ->
+                client.bulkAsync(bulkRequest, RequestOptions.DEFAULT, bulkResponseActionListener), bulkProcessorListener)
+                .setBulkActions(config.getInt("elastic.bulk.size"))
+                .setConcurrentRequests(config.getInt("elastic.concurrent.requests"))
+                .setFlushInterval(TimeValue.timeValueMinutes(config.getInt("elastic.bulk.flush.interval.seconds")))
+                .setBackoffPolicy(BackoffPolicy.constantBackoff(
+                        TimeValue.timeValueSeconds(config.getLong("elastic.backoff.delay.seconds")),
+                        config.getInt("elastic.backoff.retries")));
         bulkProcessor = bulkProcessorBuilder.build();
-    }
 
-    private RestHighLevelClient getClient() {
-        if (restHighLevelClient == null) {
-            restHighLevelClient = new RestHighLevelClient(
-                    RestClient.builder(new HttpHost(hostname, port)));
-        }
-        return restHighLevelClient;
+        MetricRegistry metricRegistry = SharedMetricRegistries.getDefault();
+        deleteTimer = metricRegistry.timer("elastic-delete");
+        insertionTimer = metricRegistry.timer("elastic-insertion");
+        elasticFailureMeter = metricRegistry.meter("elastic-insertion-failure");
+        bulkInsertionMeter = metricRegistry.meter("elastic-bulk-insertion");
+        bulkInsertionTimer = metricRegistry.timer("elastic-bulk-insertion-timer");
+        bulkInsertionFailures = metricRegistry.meter("elastic-bulk-insertion-failure");
     }
-
 
     public Site get(String url) {
         GetRequest getRequest = new GetRequest(index, url);
         try {
-            GetResponse response = getClient().get(getRequest, RequestOptions.DEFAULT);
+            GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
             if (response.isExists()) {
                 return new Site(
                         response.getId(),
                         response.getSourceAsMap().get("title").toString());
             } else {
+                logger.warn(String.format("Elastic found no match id for [%s]", url));
                 return null;
             }
         } catch (IOException e) {
@@ -122,6 +125,7 @@ public class ElasticSiteDaoImpl implements SiteDao, Closeable {
             builder.endObject();
             IndexRequest indexRequest = new IndexRequest(index).id(site.getLink()).source(builder);
             bulkProcessor.add(indexRequest);
+            logger.trace(String.format("Elastic Inserted [%s]", site.getLink()));
         } catch (IOException e) {
             elasticFailureMeter.mark();
             throw new ElasticSiteDaoException(String.format("Elastic couldn't insert [%s]", site.getLink()), e);
@@ -132,10 +136,23 @@ public class ElasticSiteDaoImpl implements SiteDao, Closeable {
     public void delete(String url) {
         DeleteRequest deleteRequest = new DeleteRequest(index, url);
         try (Timer.Context time = deleteTimer.time()) {
-            getClient().delete(deleteRequest, RequestOptions.DEFAULT);
-            logger.debug(String.format("Link [%s] deleted from elastic", url));
+            client.delete(deleteRequest, RequestOptions.DEFAULT);
+            logger.trace(String.format("Link [%s] deleted from elastic", url));
         } catch (IOException e) {
             logger.error(String.format("Elastic couldn't delete [%s]", url), e);
+        }
+    }
+
+
+    @Override
+    public void close() throws IOException {
+        try {
+            logger.trace("Shut down operation in elastic started ...");
+            bulkProcessor.awaitClose(elasticBulkTimeOut, TimeUnit.SECONDS);
+            client.close();
+            logger.trace("Shut down operation in elastic completed.");
+        } catch (InterruptedException e) {
+            logger.error("Thread interrupted in elastic close.", e);
         }
     }
 
@@ -146,19 +163,11 @@ public class ElasticSiteDaoImpl implements SiteDao, Closeable {
                 .put("index.number_of_replicas", 1)
         );
         try {
-            getClient().indices().create(request, RequestOptions.DEFAULT);
+            client.indices().create(request, RequestOptions.DEFAULT);
         } catch (IOException e) {
             logger.error("Elastic Can't Create Index", e);
         }
     }
 
-    @Override
-    public void close() throws IOException {
-        try {
-            bulkProcessor.awaitClose(config.getInt("elastic.bulk.timeout"), TimeUnit.SECONDS);
-            getClient().close();
-        } catch (InterruptedException e) {
-            logger.error("thread interrupted in elastic close.", e);
-        }
-    }
 }
+
