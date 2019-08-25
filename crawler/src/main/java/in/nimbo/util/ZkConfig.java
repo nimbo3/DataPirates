496845bcd638b7ca49dedee4566d0ddd4de85a04
MostafaOjaghi <m.ojaghi79@gmail.com>
2019-08-25 15:59:02 +0430

    change config structure and add config overriding

--- crawler/src/main/java/in/nimbo/util/ZkConfig.java
+++ crawler/src/main/java/in/nimbo/util/ZkConfig.java
@@ -2,19 +2,22 @@ package in.nimbo.util;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigParseOptions;
-import com.typesafe.config.ConfigSyntax;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 public class ZkConfig extends Observable<Config> {
     private static Logger logger = LoggerFactory.getLogger(ZkConfig.class);
+    private Map<String, String> previousConfigMap = new HashMap<>();
     private Config config;
     private ZooKeeper zookeeper;
 
@@ -45,23 +48,84 @@ public class ZkConfig extends Observable<Config> {
 
     private void updateConfig() {
         try {
-            setState(getConfigFromZk());
+            Map<String, String> newConfig = getConfigMapFromZk();
+            if (!previousConfigMap.equals(newConfig)) {
+                previousConfigMap = newConfig;
+                setState(ConfigFactory.parseMap(newConfig));
+            }
         } catch (KeeperException e) {
             logger.error("ZooKeeper get exception", e);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            logger.error("interrupted!", e);
             Thread.currentThread().interrupt();
         }
     }
 
-    private Config getConfigFromZk() throws KeeperException, InterruptedException {
-        String configString = new String(zookeeper.getData("/crawler/config",
-                event -> updateConfig(), null));
-        return ConfigFactory.parseString(configString,
-                ConfigParseOptions.defaults().setSyntax(ConfigSyntax.PROPERTIES));
+    private Map<String, String> getConfigMapFromZk() throws KeeperException, InterruptedException {
+        List<String> children = zookeeper.getChildren("/crawler", event -> updateConfig());
+        Map<String, String> configMap = getConfigMapFromPath("/crawler");
+        try {
+            String hostName = InetAddress.getLocalHost().getHostName();
+            if (children.contains(hostName))
+                configMap.putAll(getConfigMapFromPath("/crawler/" + hostName));
+        } catch (UnknownHostException e) {
+            logger.error("can't get machine name", e);
+        }
+        return configMap;
+    }
+
+    private Map<String, String> getConfigMapFromPath(String path) throws KeeperException, InterruptedException {
+        if (!zookeeper.getChildren(path, event -> updateConfig()).contains("config"))
+            return new HashMap<>();
+        List<String> configList = zookeeper.getChildren(path + "/config", event -> updateConfig());
+        Map<String, String> configMap = new HashMap<>();
+        configList.forEach(configName -> {
+            try {
+                String configValue = new String(zookeeper.getData(path + "/config/" + configName,
+                        event -> updateConfig(), null));
+                configMap.put(configName, configValue);
+            } catch (KeeperException e) {
+                logger.error("can't get config from zookeeper", e);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        return configMap;
     }
 
     public Config getConfig() {
         return getState();
+    }
+
+    /**
+     * path should exist in zookeeper
+     */
+    public void initInZooKeeper(String resource, String path) {
+        ConfigFactory.parseResourcesAnySyntax(resource).entrySet().forEach(stringConfigValueEntry -> {
+            try {
+                Stat stat = new Stat();
+                zookeeper.getData(path + "/config/" + stringConfigValueEntry.getKey(), null, stat);
+                zookeeper.setData(path + "/config/" + stringConfigValueEntry.getKey(),
+                        ((String) stringConfigValueEntry.getValue().unwrapped()).getBytes(), stat.getVersion());
+            } catch (KeeperException e) {
+                logger.info("couldn't write on zookeeper. try to create new node.");
+                try {
+                    zookeeper.create(path + "/config/" + stringConfigValueEntry.getKey(),
+                            ((String) stringConfigValueEntry.getValue().unwrapped()).getBytes(),
+                            ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                } catch (KeeperException e2) {
+                    logger.error("couldn't write on zookeeper", e);
+                } catch (InterruptedException e2) {
+                    Thread.currentThread().interrupt();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    public static void main(String[] args) throws IOException {
+        ZkConfig zkConfig = new ZkConfig(ConfigFactory.parseResourcesAnySyntax("config"));
+        zkConfig.initInZooKeeper("config", "/crawler");
     }
 }
