38b8f1100ae4b6f3af5708775f9627cbbc48d3ca
MostafaOjaghi <m.ojaghi79@gmail.com>
2019-08-28 11:52:29 +0430

    change result controller

--- backend/src/main/java/in/nimbo/SearchResultController.java
+++ backend/src/main/java/in/nimbo/SearchResultController.java
@@ -8,6 +8,8 @@ import com.typesafe.config.ConfigFactory;
 import in.nimbo.model.ResultEntry;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -15,16 +17,10 @@ public class SearchResultController {
 
     @CrossOrigin
     @GetMapping("/search")
-    public List<ResultEntry> greeting(@RequestParam(value = "input") String input, @RequestParam(value = "type", defaultValue = "2") int type) {
+    public List<ResultEntry> greeting(@RequestParam(value = "query") String query, @RequestParam(value = "lang") String lang) {
         Config config = ConfigFactory.load("config");
         ElasticSearch elasticSearch = new ElasticSearch(config);
-        if (type == 1)
-            return elasticSearch.search(input);
-        else if (type == 2)
-            return elasticSearch.multiMatchSearch(input);
-        else if (type == 3)
-            return elasticSearch.fuzzySearch(input);
-        throw new IllegalArgumentException();
+        return Arrays.asList(new ResultEntry("title", "example.com", "hello!!", 2, Arrays.asList("12", "13")));
     }
 }
 
