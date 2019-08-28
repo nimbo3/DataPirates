fecbb62a1ce56a02011d990d8d42d5ba62641176
kazemf78 <kazemf78@yahoo.com>
2019-08-28 20:43:25 +0430

    save data as text file

--- page-rank/src/main/java/in/nimbo/App.java
+++ page-rank/src/main/java/in/nimbo/App.java
@@ -7,15 +7,12 @@ import in.nimbo.util.CellUtility;
 import in.nimbo.util.HashGenerator;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -25,7 +22,6 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.storage.StorageLevel;
 import scala.Tuple2;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -88,6 +84,7 @@ public class App {
 
         Set hashedRowKeys = new HashSet();
         edges.foreach(edge -> {
+//            System.out.println(edge._1 + " :: " + edge._2);
             String hashedRowKey = new String(DigestUtils.md5(edge._1), StandardCharsets.UTF_8);
             hashedRowKeys.add(hashedRowKey);
         });
@@ -96,27 +93,34 @@ public class App {
 
         Set broadcastHashSet = broadcastVar.getValue();
         JavaPairRDD<String, String> validEdges = edges.filter(edge -> broadcastHashSet.contains(HashGenerator.md5HashString(edge._2)));
+        validEdges.foreach(t -> System.out.println(t._1 + " <-> " + t._2));
         broadcastVar.destroy();
 
         JavaPairRDD<String, Iterable<String>> links = validEdges.groupByKey().cache();
         JavaPairRDD<String, Double> ranks = links.mapValues(rs -> 1.0);
 
         for (int current = 0; current < pageRankIterNum; current++) {
+            final int finalCurrent = current;
             JavaPairRDD<String, Double> contribs = links.join(ranks).values()
                     .flatMapToPair(s -> {
+                        System.out.println("current_iteration: " + finalCurrent + ", url: " + s._1 + " -> rank: " + s._2);
                         int urlCount = Iterables.size(s._1());
                         List<Tuple2<String, Double>> results = new ArrayList<>();
                         for (String n : s._1) {
-                            results.add(new Tuple2<>(n, s._2() / urlCount));
+                            results.add(new Tuple2<>(n, s._2 / urlCount));
                         }
                         return results.iterator();
                     });
+            System.out.println("current_iteration: " + finalCurrent);
             ranks = contribs.reduceByKey(Double::sum).mapValues(sum -> 0.15 + sum * 0.85);
         }
+
+//        ranks.collect();
 
         byte[] familyBytes = Bytes.toBytes(hbaseResultColumnFamily);
         byte[] qualifierBytes = Bytes.toBytes(hbaseResultColumnQualifier);
         JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = ranks.mapToPair(t -> {
+            System.out.println("link: " + t._1 + " -> rank: " + t._2);
             byte[] urlBytes = Bytes.toBytes(t._1);
             byte[] rankBytes = Bytes.toBytes(t._2);
             Put hbasePut = new Put(urlBytes);
@@ -124,21 +128,23 @@ public class App {
             return new Tuple2<>(new ImmutableBytesWritable(), hbasePut);
         });
 
-        Configuration hbaseWriteConfiguration = HBaseConfiguration.create();
-        hbaseWriteConfiguration.addResource(hbaseXmlHadoop);
-        hbaseWriteConfiguration.addResource(hbaseXmlHbase);
-        hbaseWriteConfiguration.set(TableInputFormat.INPUT_TABLE, hbaseResultTableName);
-
-        try {
-            Job jobConfig = Job.getInstance(hbaseWriteConfiguration);
-            jobConfig.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, hbaseResultTableName);
-            jobConfig.setOutputFormatClass(TableOutputFormat.class);
-
-            hbasePuts.saveAsNewAPIHadoopDataset(jobConfig.getConfiguration());
-        } catch (IOException e) {
-            // do sth
-            System.out.println("couldn't insert into hbase :((");
-        }
+        hbasePuts.saveAsTextFile("/kafa22222");
+//
+//        Configuration hbaseWriteConfiguration = HBaseConfiguration.create();
+//        hbaseWriteConfiguration.addResource(hbaseXmlHadoop);
+//        hbaseWriteConfiguration.addResource(hbaseXmlHbase);
+//        hbaseWriteConfiguration.set(TableInputFormat.INPUT_TABLE, hbaseResultTableName);
+//
+//        try {
+//            Job jobConfig = Job.getInstance(hbaseWriteConfiguration);
+//            jobConfig.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, hbaseResultTableName);
+//            jobConfig.setOutputFormatClass(TableOutputFormat.class);
+//
+//            hbasePuts.saveAsNewAPIHadoopDataset(jobConfig.getConfiguration());
+//        } catch (IOException e) {
+//            // do sth
+//            System.out.println("couldn't insert into hbase :((");
+//        }
 //        JavaRDD<UpdateObject> elasticRDD = null;
 //        JavaEsSpark.saveToEs(elasticRDD, elasticIndexName);
 
