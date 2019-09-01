980a881580d57d4e0acdf2be5c1e2403f46a09c5
Hamidreza Sharifzadeh <hamidrezash2000@gmail.com>
2019-09-01 13:55:06 +0430

    handle advanced search options in query

--- backend/src/main/java/in/nimbo/SearchResultController.java
+++ backend/src/main/java/in/nimbo/SearchResultController.java
@@ -3,16 +3,19 @@ package in.nimbo;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import in.nimbo.model.ResultEntry;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class SearchResultController {
-
-    private static Config config = ConfigFactory.load("config");
+    Config config = ConfigFactory.load("config");
+    ElasticSearch elasticSearch = new ElasticSearch(config);
 
     @CrossOrigin
     @GetMapping("/search")
@@ -31,17 +34,14 @@ public class SearchResultController {
     @CrossOrigin
     @GetMapping("/api/autocomplete")
     public List<String> autoComplete(@RequestParam(value = "input") String input, @RequestParam(value = "lang") String lang) {
-        ElasticSearch elasticSearch = new ElasticSearch(config);
         try {
-            List<String> autoComplete = elasticSearch.autoComplete(input, lang.toLowerCase());
-            return autoComplete;
+            if (input.contains("domain:") || input.contains("-") || input.contains("\""))
+                return new ArrayList<>();
+            else
+                return elasticSearch.autoComplete(input, lang.toLowerCase());
         } catch (IOException e) {
             e.printStackTrace();
-            return Arrays.asList(
-                    "salam",
-                    "khoobi",
-                    "chetori"
-            );
+            return new ArrayList<>();
         }
     }
 }
